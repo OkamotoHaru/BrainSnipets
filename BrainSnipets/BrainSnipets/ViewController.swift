7d1d03f47150808486f99371329e1711a2da0a09
OkamotoHaru <truevr.lisbeth@gmail.com>
2019-08-12 14:03:18 +0900

    ## 対応 * hello world表示 * アプリアイコン設定

---
+++ BrainSnipets/BrainSnipets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrainSnipets
+//
+//  Created by masato on 2019/08/12.
+//  Copyright © 2019年 FreeNerdLaughed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BrainSnipets/BrainSnipetsTests/BrainSnipetsTests.swift
@@ -0,0 +1,34 @@
+//
+//  BrainSnipetsTests.swift
+//  BrainSnipetsTests
+//
+//  Created by masato on 2019/08/12.
+//  Copyright © 2019年 FreeNerdLaughed. All rights reserved.
+//
+
+import XCTest
+@testable import BrainSnipets
+
+class BrainSnipetsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
